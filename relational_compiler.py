ce696d1fbd03abe55fb0166749c9c11d43eec453
Phil Owen <phillipsowen@outlook.com>
2019-08-06 15:31:33 -0400

    isolating errant column parsing into sql statement

--- relational_compiler.py
+++ relational_compiler.py
@@ -106,7 +106,7 @@ class RelationalCompiler(BagCompiler):
                            for i, v in enumerate(values):
                                print(f" {i} - {v}")
                                j+=1
-                       return None
+                           return None
                except:
                    print (values)
                    for i, v in enumerate(values):
